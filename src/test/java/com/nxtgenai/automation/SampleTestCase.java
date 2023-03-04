2709ed8337a0a9232206e62483435473be7ffe05
Vinoth R <vinothrwins@gmail.com>
2023-03-05 02:38:20 +0530

    Adding the new maven project

---
+++ src/test/java/com/nxtgenai/automation/SampleTestCase.java
@@ -0,0 +1,11 @@
+package com.nxtgenai.automation;
+
+import org.testng.annotations.Test;
+
+public class SampleTestCase {
+	
+  @Test
+  public void sampleTestCase() {
+	  System.out.println("Test Case Executed");
+  }
+}
